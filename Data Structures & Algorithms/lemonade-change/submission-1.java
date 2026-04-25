91e41dc668644ef099c2dc89ef1ec9a1c8311a2d
genej88he <genej88@uw.edu>
2026-04-25 00:14:53 -0700

    Add: lemonade-change - submission-1

---
+++ Data Structures & Algorithms/lemonade-change/submission-1.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int[] possibleBills = new int[3];
+        for (int i = 0; i < bills.length; i++) {
+            int thisNumFives = bills[i] / 5;
+            System.out.println(thisNumFives);    
+            if (thisNumFives == 1) {
+                possibleBills[0]++;
+            } else if (thisNumFives == 2) {
+                possibleBills[1]++;
+                if (possibleBills[0] == 0) {
+                    return false;
+                } else {
+                    possibleBills[0]--;
+                }
+            } else {
+                possibleBills[2]++;
+                if (possibleBills[1] == 0 && possibleBills[0] < 3) {
+                    return false;
+                }
+                if (possibleBills[1] != 0 && possibleBills[0] == 0) {
+                    return false;
+                }
+                
+                if (possibleBills[1] == 0) {
+                    possibleBills[0] -= 3;
+                } else {
+                    possibleBills[0]--;
+                    possibleBills[1]--;
+                }
+            }
+        }
+        return true;
+    }
+}
