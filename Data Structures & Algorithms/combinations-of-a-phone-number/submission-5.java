4f7ac94afa02e7a45df0a537b4dfeb22c76230d5
genej88he <genej88@uw.edu>
2026-05-13 23:29:31 -0700

    Add: combinations-of-a-phone-number - submission-5

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-5.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        Map<Character, String> map = new HashMap<>();
+        map.put('2', "abc");
+        map.put('3', "def");
+        map.put('4', "ghi");
+        map.put('5', "jkl");
+        map.put('6', "mno");
+        map.put('7', "pqrs");
+        map.put('8', "tuv");
+        map.put('9', "wxyz");
+        List<String> result = new ArrayList<>();
+        if (digits.length() == 0) {
+            return result;
+        }
+        backtrack(digits, map, "", 0, result);
+        return result;
+    }
+
+    private void backtrack(String digits, Map<Character, String> map, String soFar, int index, List<String> result) {
+        if (index != digits.length()) {
+            System.out.println(soFar);
+            Character thisIndex = digits.charAt(index);
+            String correlated = map.get(thisIndex);
+            int length = soFar.length();
+            for (int i = 0; i < correlated.length(); i++) {
+                soFar += correlated.charAt(i);
+                backtrack(digits, map, soFar, index + 1, result);
+                soFar = soFar.substring(0, length);
+            }
+        } else {
+            if (soFar.length() == digits.length()) {
+                result.add(soFar);
+            }
+        }
+        
+        
+    }
+}
