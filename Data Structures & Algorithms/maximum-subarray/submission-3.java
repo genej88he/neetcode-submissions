78badc3c9015e27d93b1feaadbd60cd63167ddc9
genej88he <genej88@uw.edu>
2026-05-14 17:43:58 -0700

    Add: maximum-subarray - submission-3

---
+++ Data Structures & Algorithms/maximum-subarray/submission-3.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int curr = 0;
+        // List<Integer> runningList = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            curr += nums[i];
+            max = Math.max(curr, max);
+            if (curr < 0) {
+                curr = 0;
+            }
+        }
+        return max;
+    }
+}
