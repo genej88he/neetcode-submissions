e42308b37622b80072775b2d9b4591cb68ec10f7
genej88he <genej88@uw.edu>
2026-05-19 00:48:06 -0700

    Add: jump-game - submission-2

---
+++ Data Structures & Algorithms/jump-game/submission-2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int length = nums.length - 1;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] + i >= length) {
+                length = i;
+            }
+        }
+
+        if (length == 0) {
+            return true;
+        } else {
+            return false;
+        }
+        
+        
+    }
+}
