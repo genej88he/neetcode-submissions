4747066e0bd837ac5331195a30f725c2324a00f6
genej88he <genej88@uw.edu>
2026-05-21 01:07:57 -0700

    Add: gas-station - submission-1

---
+++ Data Structures & Algorithms/gas-station/submission-1.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int gasTotal = 0;
+        int costTotal = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+            gasTotal += gas[i];
+            costTotal += cost[i];
+        } 
+        if (gasTotal < costTotal) {
+            return -1;
+        }
+        gasTotal = 0;
+        costTotal = 0;
+        int times = 0;
+        int size = gas.length;
+        for (int i = 0; i <= size; i++) {
+            gasTotal += gas[i % gas.length];
+            costTotal += cost[i % gas.length];
+            System.out.println(gasTotal);
+            System.out.println(costTotal);
+            System.out.println("---");
+            
+            if (gasTotal < costTotal) {
+                gasTotal = 0;
+                costTotal = 0;
+                size++;
+                times = 0;
+                continue;
+            }
+            if (times == gas.length) {
+                return i % gas.length;
+            }
+            size++;
+            times++;
+        }
+        return -1;
+
+    }
+}
